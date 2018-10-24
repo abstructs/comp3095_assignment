400601b0e6d5d188105b67e66bf4fd736683d5c2
Justin Rolnick <justin.a.rolnick@gmail.com>
2018-10-24 15:26:40 -0400

    working on login form and servlet

--- src/loginServlets/Login.java
+++ src/loginServlets/Login.java
@@ -1,41 +1,61 @@
 package loginServlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Login
- */
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Login() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		
+		
+//		Invalid username and/or password combination is provided
+//			- occurs when a user enters an invalid username/password combination.
+//		A User requests a page that requires a user to be successfully logged in first
+//			- occurs when a user explicitly enters the URL of a restricted page without 
+//			first authenticating using the Login Servlet.
+		
+		
+		// if register button is clicked
+		if(request.getParameter("register") != null) {
+			  response.sendRedirect("Register.html");
+		};
+
+		
+		pw.println("<!DOCTYPE html>");
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<title>");
+		pw.println("Lab 2");
+		pw.println("</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<h2>");
+		pw.println("");
+		pw.println("</h2>");
+		pw.println("</body>");
+		pw.println("</html>");
 	}
 
 }
