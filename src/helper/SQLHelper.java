a130a429262b02bc18565419511a1f45b6f363a1
abstructs <andrewclarke540@gmail.com>
2018-10-24 22:48:45 -0400

    update validate password to work with hashes

--- src/helper/SQLHelper.java
+++ src/helper/SQLHelper.java
@@ -2,6 +2,7 @@ package helper;
 
 import java.sql.*;
 import helper.DatabaseAccess;
+import helper.HelperUtility;
 
 public class SQLHelper {
 	
@@ -55,9 +56,10 @@ public class SQLHelper {
 			
 			rs.next();
 			
-			String databasePassword = (String) rs.getObject("password");
+			String passwordHash = HelperUtility.sha256(password, "salt");
+			String databasePasswordHash = (String) rs.getObject("password");
 			
-			return password.equals(databasePassword);
+			return databasePasswordHash.equals(passwordHash);
 		} catch(Exception e) {
 			return false;
 		}
